faeacd7020f1bcc0c5e28481d5ad25663f41326f
navither <482033387@qq.com>
2025-01-19 12:09:39 +0800

    开始结束

--- Assets/Rhythm Game Tutorial/Scripts/GameManager.cs
+++ Assets/Rhythm Game Tutorial/Scripts/GameManager.cs
@@ -7,7 +7,12 @@ public class GameManager : MonoBehaviour
     [Tooltip("主要的背景音乐，从游戏开始播放")]
     public AudioSource audioSource;
 
+    public GameObject noteHolder;
+    public GameObject ring;
+
     bool isPlaying = false;
+
+    bool gameOver = false;
 
     public static float beginTime = 0;
 
@@ -17,6 +22,13 @@ public class GameManager : MonoBehaviour
             isPlaying = true;
             beginTime = Time.time;
             GameEvents.StartGame?.Invoke();
+        }
+
+        if(audioSource.isPlaying == false && isPlaying && !gameOver) {
+            GameEvents.EndGame?.Invoke();
+            noteHolder.SetActive(false);
+            ring.SetActive(false);
+            gameOver = true;
         }
     }
 }
